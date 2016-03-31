cc6289e01c75792d7a4f2702f2873d6cd7d62adf
jonrmulholland <mulholland_jonathan@bah.com>
2016-03-31 08:45:04 -0400

    This is the Kaggle forum version of this script.

--- dsbAnalysis.py
+++ dsbAnalysis.py
@@ -6,11 +6,22 @@ from scipy import stats
 import matplotlib.pyplot as plt
 import random
 
+###########
+#
+# 
+#  This script requires the studies in the submission files to be ordered in the same manner
+#  as the file with the true volume values (answers.csv). This is an additional requirement
+#  on top of the requirements for the submission files for the Kaggle scoring. 
+#
+###########
 
-answerFile = "answers.csv"
 
-print "splash screen"
-
+answerFile = "answers.csv" #ordered list of true values for the studies
+#  Format
+#  ID,Volume
+#  case#_Diastole, true diastolic volume
+#  case#_Systloe,  true systolic volume  
+#
 ###############
 #   
 #   Read in answers. Format is same as submission with single colume of volumes
@@ -30,31 +41,49 @@ for line in readera:
             dv_true.append(float(line[1]))
     i_line+=1
     
-sv_true = np.array(sv_true)
+##############
+# arrays storing the true volume values-- idexed by
+# order of appearance in ansers.csv
+#
+sv_true = np.array(sv_true) 
 dv_true = np.array(dv_true)
 fa.close() 
 
-#top ten
-#subFName = ["show_me_the_money.csv","heart.csv","WeiDong_YuanfangGuan.csv","TenciaWoshialex.csv","RatherBeFishing.csv","kunsthart.csv","Plankton.csv","RyanMunion.csv","sinafz.csv","JuliandeWit.csv"]
-#subTName = ["Show Me The Money","heart","Wei Dong Yuanfang Guan","Tencia Woshialex","RatherBeFishing","kunsthart","Plankton","Ryan Munion","sinafz","JuliandeWit"]
-#medianNotMode = [0,1,0,0,0,0,0,1,0,0]
-
-# top 4
-path = "submissions/"
-subTeamName = ["#1 Tencia & Woshialex","#2 kunsthart","#3 Julian de Wit","#4 show_me_the_money"]
-subInFile = ["TenciaWoshialex.csv","kunsthart.csv","JuliandeWit.csv","show_me_the_money.csv"]
-subOutFile = ["Tencia_n_Woshialex","kunsthart","Julian_de_Wit","show_me_the_money"]
-medianNotMode = [0,1,1,0]
+####################
+#
+#  Reading in competition submission files
+#  
+#  Arrays store file input and output names as well as team names
+#  use for headers and titles on plots
+#
+path = "./"
+subTeamName = ["Team 1","Team 2","Team 3","Team 4","Team 5"]
+subInFile = ["submission1.csv","submission2.csv","submission3.csv","submission4.csv","submission5.csv"]
+subOutFile = ["Team_1","Team_2","Team_3","Team_4","Team_5"]
 subInFile = [path+x for x in subInFile]
 
-errorTable = np.zeros((len(subInFile),3 )) #table for avg error, stdev of error
-accuracyTable = np.zeros((len(subInFile),2,2 )) #for each submission 2X2 diastolic
 
+
+
+######
+#
+#  Random list of entries for displaying PDFs
+#
+np.random.seed()
+eventList = random.sample(range(440), 100)
+
+####################
+#
+#  Array for storing the RMS errors for each entry
+#
+errorTable = np.zeros((len(subInFile),3 )) #table for avg error, stdev of error
+
+
+# Begin loop over submission files
 for i_sub in range(len(subInFile)):
 
-
     print "Processing %s" % subTeamName[i_sub]
-    submissionNum = i_sub+1
+
     #########################
     #
     #   Read in the submission
@@ -78,50 +107,45 @@ for i_sub in range(len(subInFile)):
     #
     #  arrays containing all cdfs for each submission
     #
-    sv_cdf = np.vstack(sv_cdf)    # index one indicates entry number
-    dv_cdf = np.vstack(dv_cdf)    # index two is the CDF index
-    n_cdf_bins = len(sv_cdf[0]) 
+    sv_cdf = np.vstack(sv_cdf)    # first index is the study
+    dv_cdf = np.vstack(dv_cdf)    # second index is the CDF value (index from 0 to 599)
+    n_cdf_bins = len(sv_cdf[0])   # number of values in each CDF
     fs.close() 
 
+    
+    n_entry = len(sv_cdf)                       # number of studies in submission files
+    dv_err = np.zeros(n_entry)                  # diastolic volume error, indexed by study
+    sv_err = np.zeros(n_entry)                  # systolic volume error, indexed by study
+    dv_conf = np.zeros(n_entry)                 # standard deviation of diastolic volume PDF (ie confidence estimate), indexed by study
+    sv_conf = np.zeros(n_entry)                 # standard deviation of systolic volume PDF (ie confidence estimate), indexed by study
+    ef_true = (dv_true - sv_true)/dv_true       # true ejection fraction calculated from the answer.csv file
+    EV_EF =  np.zeros(n_entry)                  # predicted ejection fraction value, indexed by study (EV is expectation value)
+    ef_x = np.arange(0.01,0.99,0.01)            # points at which we'll calculate ejection fraction PDF
 
-    n_entry = len(sv_cdf)
-    dv_err = np.zeros(n_entry)
-    sv_err = np.zeros(n_entry)
-    dv_conf = np.zeros(n_entry)
-    sv_conf = np.zeros(n_entry)
-    dvsv_cov = np.zeros(n_entry)
-    ef_pred = np.zeros(n_entry)
-    ef_true = (dv_true - sv_true)/dv_true
-    ef_x = np.arange(0.01,0.99,0.01) # points at which we'll calculate ejection fraction pdf
-    #ef_pdf = np.zeros((n_entry,len(ef_x)))    leaving this out for now
 
-    dv_maxiTemp = np.zeros(n_entry)
-    sv_maxiTemp = np.zeros(n_entry)
-    dv_meanTemp = np.zeros(n_entry)
-    sv_meanTemp = np.zeros(n_entry)
-    dv_pdfTemp = np.zeros((n_entry,len(dv_cdf[0])-1))
-    sv_pdfTemp = np.zeros((n_entry,len(sv_cdf[0])-1))
+    ##########
+    #
+    # Arrays for accessing pdfs and prediction values on a study by study basis
+    #
+    dv_pdf_arr = np.zeros((n_entry,len(dv_cdf[0])-1))
+    sv_pdf_arr = np.zeros((n_entry,len(sv_cdf[0])-1))
     sv_pred_arr = np.zeros(n_entry)
     dv_pred_arr = np.zeros(n_entry)
 
 
+    ####################
+    #
+    #  Looping over entries in submission file
 
+    
     for N in range(n_entry):
-        dv_pdf_max = 0.0
-        dv_pdf_mean = 0.0
-        dv_pdf_median = 0.0
-        dv_pdf_median_i = 0
-        dv_pdf_maxi = 0
-        dv_pdf_var = 0.0
-        sv_pdf_max = 0.0
-        sv_pdf_mean = 0.0
-        sv_pdf_maxi = 0
+        dv_pdf_mean = 0.0  # expectation value of DV PDF
+        dv_pdf_var = 0.0   # standard deviation of DV PDF
+        sv_pdf_mean = 0.0  
         sv_pdf_var = 0.0
-        sv_pdf_median = 0.0
-        sv_pdf_median_i = 0
         
-        sv_pred = 0.0
-        dv_pred = 0.0
+        sv_pdf_mean = 0.0
+        dv_pdf_mean = 0.0
 
         #"differentiating" the cdf
         dv_pdf = np.array([dv_cdf[N][i]-dv_cdf[N][i-1] for i in range(1,n_cdf_bins) ],dtype=np.float)
@@ -131,48 +155,16 @@ for i_sub in range(len(subInFile)):
         for i in range(len(dv_pdf)):
             dv_pdf_mean += dv_pdf[i]*float(i+0.5) # note : value of dv at index 0 is 1 ml
             sv_pdf_mean += sv_pdf[i]*float(i+0.5)
-            if medianNotMode[i_sub]:
-                dv_pdf_median += dv_pdf[i]
-                sv_pdf_median += sv_pdf[i]
-                if sv_pdf_median_i == 0.0 and sv_pdf_median >=0.5:
-                    sv_pdf_median_i = i+0.5
-                if dv_pdf_median_i == 0.0 and dv_pdf_median >=0.5:
-                    dv_pdf_median_i = i+0.5                    
-            else:
-                if dv_pdf_max < dv_pdf[i]:
-                    dv_pdf_max = dv_pdf[i]
-                    dv_pdf_maxi = i+0.5                   #(remember volume is 1+index in pdf)
-                if sv_pdf_max < sv_pdf[i]:
-                    sv_pdf_max = sv_pdf[i]
-                    sv_pdf_maxi = i+0.5
-        if medianNotMode[i_sub]:
-            sv_pdf_maxi = sv_pdf_median_i
-            dv_pdf_maxi = dv_pdf_median_i
-        dv_maxiTemp[N] = dv_pdf_maxi
-        sv_maxiTemp[N] = sv_pdf_maxi
-        dv_meanTemp[N] = dv_pdf_mean
-        sv_meanTemp[N] = sv_pdf_mean
-        dv_pdfTemp[N] = dv_pdf
-        sv_pdfTemp[N] = sv_pdf
+        dv_pdf_arr[N] = dv_pdf
+        sv_pdf_arr[N] = sv_pdf
 
-        #########
-        #ommitting calculating the norm...
-        #I'm assuming kaggle rejects submissions with non-normalized CDFs
-        ##########
-        #
-        #  who knows what the prediction should be? we'll take the average of the
-        #  pdf peak and the expectation value of the distribution
-        #
-        dv_pred = (float(dv_pdf_maxi) + dv_pdf_mean)/2.   
-        sv_pred = (float(sv_pdf_maxi) + sv_pdf_mean)/2.
-        dv_pred_arr[N] = dv_pred
-        sv_pred_arr[N] = sv_pred
-        ef_pred[N] = (dv_pred - sv_pred)/dv_pred
-        dv_err[N] = dv_pred - dv_true[N]     #absolute error in volume
-        sv_err[N] = sv_pred - sv_true[N]
+        dv_pred_arr[N] = dv_pdf_mean
+        sv_pred_arr[N] = sv_pdf_mean
+        dv_err[N] = dv_pdf_mean - dv_true[N]     #absolute error in volume
+        sv_err[N] = sv_pdf_mean - sv_true[N]
         for i in range(len(dv_pdf)):    # loop for calculating variance of pdf around prediction
-            dv_pdf_var += dv_pdf[i]*(float(i+0.5)-dv_pred)*(float(i+0.5)-dv_pred)
-            sv_pdf_var += sv_pdf[i]*(float(i+0.5)-sv_pred)*(float(i+0.5)-sv_pred)        
+            dv_pdf_var += dv_pdf[i]*(float(i+0.5)-dv_pdf_mean)*(float(i+0.5)-dv_pdf_mean)
+            sv_pdf_var += sv_pdf[i]*(float(i+0.5)-sv_pdf_mean)*(float(i+0.5)-sv_pdf_mean)        
         ##########
         #
         #  Defining the confidence of this prediction to be the stdev of pdf about the
@@ -181,42 +173,47 @@ for i_sub in range(len(subInFile)):
         dv_conf[N] = np.sqrt(dv_pdf_var)
         sv_conf[N] = np.sqrt(sv_pdf_var)
 
-    ############
-    #
-    #  counting the number of predictions within 10% of actual volume
-    #  counting the number of predictions within 10ml of actual volume
-    #
-    dv_percErr = abs(dv_err)/dv_true 
-    dv_perc_win_10perc = float(sum(dv_percErr<=0.10))/len(dv_percErr)
-    dv_perc_win_10ml = float(sum(abs(dv_err)<=10))/len(dv_err)
-
-    sv_percErr = abs(sv_err)/sv_true 
-    sv_perc_win_10perc = float(sum(sv_percErr<=0.10))/len(sv_percErr)
-    sv_perc_win_10ml = float(sum(abs(sv_err)<=10))/len(dv_err)
-
-    accuracyTable[i_sub][0] = np.array([dv_perc_win_10perc,dv_perc_win_10ml])
-    accuracyTable[i_sub][1] = np.array([sv_perc_win_10perc,sv_perc_win_10ml])
-
+        #rv_discrete only handles integers... making a dict to translate the range into the central bin value
+        vals = np.arange(0.5,599.5,1.0)
+        valDict = {i:vals[i] for i in range(len(vals))}
+        sv_rand = stats.rv_discrete(name='sv_rand',values=(range(len(vals)),sv_pdf))  #creating a RNG using the PDF as a weighting function
+        dv_rand = stats.rv_discrete(name='dv_rand',values=(range(len(vals)),dv_pdf))
+        randSVs = sv_rand.rvs(size=10000) #sampling 10k systolic volumes 
+        randDVs = dv_rand.rvs(size=10000)
+        randSVs = [valDict[x] for x in randSVs] # converting integer to central bin values
+        randDVs = [valDict[x] for x in randDVs]
+        randEFs= [(float(x)-float(y))/float(x) for x,y in zip(randDVs,randSVs)]  # array of EFs generated from the SV and DV samples
+        (binCont,bins)=np.histogram(randEFs,bins=101,range=(0,1.0))   # histogramming the EFs
+        EF_pdf = np.array(binCont,dtype=np.float)  
+        EF_bin_cent = [(bins[i]+bins[i+1])/2.0 for i in range(len(bins)-1)]  # central values of EF bins
+        EF_pdf = EF_pdf/np.sum(EF_pdf)    # normalizing the EF PDF
+        # Calculating Expectation Value of EF  EV_EF
+        mean_EF = 0.0
+        for i in range(len(EF_bin_cent)):
+            mean_EF += EF_bin_cent[i]*EF_pdf[i]
+        EV_EF[N]=mean_EF 
 
     
     ########################
     #
-    #  generating a confusion matrix
-    #  based on Ejection Fraction values on the DSB webpage
+    #  generating a confusion/contingency matrix
+    #  based on Ejection Fraction bounds provided by Dr Arai in private correspondence
     #
     ef_clinic_bins = np.array([0.0,0.35,0.45,0.55,0.73,1.0])
     ef_clinic_bin_names = ["Sev. Abnorm.\n   <35%","Mod. Abnorm.\n 35% to 45%","Mild. Abnorm.\n 45% to 55%","Normal EF\n 55% to 73% ","Hyperdynamic\n   >73%"]
+    # these are the possible classification values of each EF. "-1" is for those that do not fall within
+    # the 0 to 100% range. They get removed from the matrix later on
     ef_clinic_bin_index = [-1,0,1,2,3,4]
     ef_pred_class = np.zeros(n_entry,dtype=np.int)
     ef_true_class = np.zeros(n_entry,dtype=np.int)
     ###########
     #
-    # looping over entries and assigning a class. -1 is out of bounds
+    # looping over studies and assigning a class. -1 is out of bounds
     #
     for N in range(n_entry):
         found_bin = 0  # did we bin this ?
         for i in range(1,len(ef_clinic_bins)):
-            if ef_pred[N] >= ef_clinic_bins[i-1] and ef_pred[N] < ef_clinic_bins[i]:
+            if EV_EF[N] >= ef_clinic_bins[i-1] and EV_EF[N] < ef_clinic_bins[i]:
                 ef_pred_class[N] = i-1
                 found_bin = 1
             if ef_true[N] >= ef_clinic_bins[i-1] and ef_true[N] < ef_clinic_bins[i]:
@@ -224,7 +221,7 @@ for i_sub in range(len(subInFile)):
         if found_bin == 0:
             ef_pred_class[N] = -1
 
-    cm_clinic = confusion_matrix(ef_true_class,ef_pred_class,ef_clinic_bin_index)
+    cm_clinic = confusion_matrix(ef_true_class,ef_pred_class,ef_clinic_bin_index) #sklearn confusion matrix 
 
     ################################
     #
@@ -239,6 +236,10 @@ for i_sub in range(len(subInFile)):
     #  Plotting all this stuff
     #
 
+    ####################
+    # 
+    # Correlation plots
+    
     #plotting prediction vs truth... both volumes
     slope, intercept, rvalue, pvalue, std_err = stats.linregress(np.append(dv_true,sv_true),np.append(dv_pred_arr,sv_pred_arr))
     plt.scatter(dv_true,dv_pred_arr,label='Diastolic Volume',marker='o',facecolors='none',edgecolors='r')
@@ -257,8 +258,8 @@ for i_sub in range(len(subInFile)):
     plt.close()
     
     #plotting prediction vs truth... EF
-    slope, intercept, rvalue, pvalue, std_err = stats.linregress(ef_true*100.0,ef_pred*100.0)
-    plt.scatter(ef_true*100.0,ef_pred*100.0,marker='x',color='#990099',label="Ejection Fraction")
+    slope, intercept, rvalue, pvalue, std_err = stats.linregress(ef_true*100.0,EV_EF*100.0)
+    plt.scatter(ef_true*100.0,EV_EF*100.0,marker='x',color='#990099',label="Ejection Fraction")
     x = np.linspace(0,90,10)
     plt.plot(x,x,color='k',label='guide y = x')
     plt.plot(x,x*slope+intercept,color = 'k',linestyle='--',label='y=%.2fx+%.2f\n$R^2=$%.3f p=%.2e' % (slope,intercept,rvalue**2,pvalue))
@@ -272,7 +273,10 @@ for i_sub in range(len(subInFile)):
     plt.savefig("%sCorrEF.png" % (subOutFile[i_sub]))
     plt.close()
 
-
+    ##################
+    #
+    #  Bland - Altman plots
+    #
     #plotting Bland-Altman for dv and sv
     avgVold = (dv_true+dv_pred_arr) /2.0
     difVold = (dv_true-dv_pred_arr)
@@ -298,9 +302,9 @@ for i_sub in range(len(subInFile)):
     plt.savefig("%sVolBlandAltman.png" % (subOutFile[i_sub]) )
     plt.close()
 
-    #plotting Bland-Altman for dv and sv
-    avgVol = (ef_true+ef_pred) /2.0
-    difVol = (ef_true-ef_pred)
+    #plotting Bland-Altman for EF
+    avgVol = (ef_true+EV_EF) /2.0
+    difVol = (ef_true-EV_EF)
     plt.scatter(avgVol,difVol,label='Ejection Fraction',marker='o',color='m')
     totalMean = np.mean(difVol)
     totalstd = np.std(difVol)
@@ -320,8 +324,13 @@ for i_sub in range(len(subInFile)):
     plt.savefig("%sEFBlandAltman.png" % (subOutFile[i_sub]) )
     plt.close()
 
+    ################
+    #
+    #
+    # Confusion Matrix Plot
+    #
+    #
     
-    # Confuction Matrix Plot
     cm_temp = cm_clinic[1:,1:] #(get rid of overflow bins)
     plt.imshow(cm_temp,interpolation='nearest',cmap=plt.cm.Reds)
     plt.title("%s\nConfusion Matrix" % (subTeamName[i_sub]))
@@ -341,39 +350,46 @@ for i_sub in range(len(subInFile)):
     plt.savefig("%scmClinicSub.png" % (subOutFile[i_sub]))
     plt.close()
 
-    #A random pdf with true values
+
+    ######################
+    #
+    #  plotting PDFs along with true SV and DV values.
+    #  100 study indices are chosen randomly and put into eventList
+    #  Here they are plotted in a large grid of plots 
+    # 
+    #
     figPDF,ax = plt.subplots(10,10,sharex=False,sharey=False,figsize=(20,20))
     figPDF.suptitle(" %s\nRandomly selected PDFS and asssociated test values\nRed : Systolic PDF        Blue : Diastolic PDF\ndashed line : True Systolic        solid line : True Diastolic" % (subTeamName[i_sub]),fontsize="20")
-    random.seed()
-    eventList = random.sample(range(440), 100)
     for i in range(10):
         for j in range(10):
             ax[i,j].tick_params(labelbottom="off",labelleft="off")
             N = eventList[j + i*10]
-            ax[i,j].plot(range(599),sv_pdfTemp[N],color='r',label="Systolic Vol PDF")
-            ax[i,j].plot(range(599),dv_pdfTemp[N],color='b',label="Diastolic Vol PDF")
-            ylimits = ax[i,j].get_ylim()
+            ax[i,j].plot(range(599),sv_pdf_arr[N],color='r',label="Systolic Vol PDF")
+            ax[i,j].plot(range(599),dv_pdf_arr[N],color='b',label="Diastolic Vol PDF")
+            ylimits = ax[i,j].get_ylim() # make sure the PDFS fit within the plot 
             Y = np.linspace(ylimits[0],ylimits[1],10)
             X = np.array([sv_true[N]]*10)
             ax[i,j].plot(X,Y,color='k',linestyle='--',label='Systolic True')
             X = np.array([dv_true[N]]*10)
             ax[i,j].plot(X,Y,color='k',label='Diastolic True')
-            ax[i,j].plot(range(599),sv_pdfTemp[N],color='r',label="Systolic Vol PDF")
-            ax[i,j].plot(range(599),dv_pdfTemp[N],color='b',label="Diastolic Vol PDF")
+            ax[i,j].plot(range(599),sv_pdf_arr[N],color='r',label="Systolic Vol PDF")
+            ax[i,j].plot(range(599),dv_pdf_arr[N],color='b',label="Diastolic Vol PDF")
             ax[i,j].set_title("index = %d" % N)
+            # for each plot I wanted the PDF and true SV and DV values to be seen as
+            #clearly as possible, so I'm fiddling with the x bounds here
             moveX = list(ax[i,j].get_xlim())
-            testX = np.max([sv_true[N],dv_true[N]])
+            testX = np.max([sv_true[N],dv_true[N]])    #
             moveX[1] = np.min([testX+75,600])
             ax[i,j].set_xlim(moveX)
     figPDF.savefig("%s100PDFs.png" % (subOutFile[i_sub]))
     plt.close()    
     
-    #    plt.title(" Example Pobability Density\n%s" % (subTeamName[i_sub]))
-    #    plt.legend(loc="upper right")
-    #    plt.xlabel("Volume in mL")
 
 
-    
+    ##########################
+    #
+    #   Scatter plot of PDF standard deviation vs absolute error of prediction
+    #
     #only do this for one submission
     if i_sub == 0:
         #########################
@@ -439,8 +455,6 @@ for i_sub in range(len(subInFile)):
         axSV.set_xlim((0,80))
         axSV.set_ylim((0,25))
         axSV.grid()
-        #    plt.savefig("%sSVScatter.png" % (subTName[i_sub]))
-        #    plt.close()
 
         axDV.scatter(abs(dv_err),dv_conf,color='#bfbfbf',s=6,marker='o', label = "All Predictions")
         axDV.scatter(abs(dv_err_best_err),dv_err_best_conf,color='c',marker='*',s=55,label='5% Smallest Abs Error')
@@ -454,27 +468,19 @@ for i_sub in range(len(subInFile)):
         axDV.set_xlim((0,80))
         axDV.set_ylim((0,25))
         axDV.grid()
-        #plt.savefig("%sDVScatter.png" % (subTName[i_sub]))
-        #plt.close()
 
-        figScat.savefig("%sAllScatter.png" %(subTeamName[i_sub]))
+        figScat.savefig("%sScatter.png" %(subTeamName[i_sub]))
         plt.close()
-
-
-
-
 
     
             
-#outputing accuracy and regression tables
-
-fout = open("accuracyTable.csv","w")
-fout.write("team name,D/S,% within 10% of truth,% within 10ml of truth\n")
-for i in range(len(subTeamName)):
-    fout.write("%s,Diastole,%.4f,%.4f\n" % (subTeamName[i],accuracyTable[i][0][0],accuracyTable[i][0][1]))
-    fout.write("%s,Systole,%.4f,%.4f\n" % (subTeamName[i],accuracyTable[i][1][0],accuracyTable[i][1][1]))
-fout.close()
-
+#################
+#
+# RMS Error table is written to a csv
+# you're on your own if you want to display
+# in a pretty fashion
+# 
+#
 
 fout = open("errorTable.csv","w")
 fout.write("team name,RMS error for D/S/EF\n")
